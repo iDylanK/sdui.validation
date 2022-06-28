933d3211bb314dc77f7e0a575859ac947bac575a
iDylanK <dylankieft@live.nl>
2022-06-28 11:15:20 +0200

    Added swiftui to delegates

--- ios/Shared/Delegate/ComponentDelegate.swift
+++ ios/Shared/Delegate/ComponentDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Dylan on 22/06/2022.
 //
 
+import SwiftUI
 import SDUI
 
 //class ComponentDelegate: SDUIComponentDelegate {}
--- ios/Shared/Delegate/HeaderDelegate.swift
+++ ios/Shared/Delegate/HeaderDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Dylan on 28/06/2022.
 //
 
+import SwiftUI
 import SDUI
 
 //class HeaderDelegate: SDUIHeaderDelegate {}
